93b3c7ccbc16e7cd78c7e12253cd2f405b9aeba7
szlakowy <jakublanda.matfil@gmail.com>
2025-10-22 18:49:22 +0200

    Add CSRF trusted origins support

--- demo/settings.py
+++ demo/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "lokalny-klucz-na-testy")
 DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "http://localhost,http://127.0.0.1").split(",")
+
 
 # Application definition
 
